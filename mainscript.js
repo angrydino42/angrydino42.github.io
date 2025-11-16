9f2e431a2e314b0e80cf82060870530463a16993
42 - Dino (AM's Wife) <angrydino42@gmail.com>
2025-11-16 19:23:08 +1300

    main website update

---
+++ mainscript.js
@@ -0,0 +1,7 @@
+function madebttnslist() {
+    if (document.getElementById("madebttns").style.display == "none") {
+        document.getElementById("madebttns").style.display = "block";
+    } else if (document.getElementById("madebttns").style.display == "block") {
+        document.getElementById("madebttns").style.display = "none";
+    }
+}
